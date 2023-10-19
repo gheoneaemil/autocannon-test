3ceae192dc7e69d31bdcb1c9c86dd2792e24d9e6
DIIMIIM <emilian@stater.co>
2023-10-20 00:13:42 +0300

    tests: first initial tests

--- server.ts
+++ server.ts
@@ -1,11 +1,23 @@
-import { port } from "./utils/params";
+import { port, totalRouteWeigth } from "./utils/params";
+import { Request, Response } from "express";
 
 const express = require('express');
 const app = express();
+let totalRequests = 0;
 
 
-app.get('/', (_, res) => {
-  res.send('Express + TypeScript Server');
+app.get('/', (_: Request, res: Response) => {
+  ++totalRequests;
+  console.log(totalRequests);
+  return res.status(200).send('Express + TypeScript Server').end();
+});
+
+app.get('/heavier', (_: Request, res: Response) => {
+    let output: Array<number> = [];
+    for ( let  i = 0 , l = totalRouteWeigth ; i < l ; ++i ) {
+        output.push(i);
+    }
+    return res.status(200).json({ output: output }).end();
 });
 
 app.listen(port, () => {
